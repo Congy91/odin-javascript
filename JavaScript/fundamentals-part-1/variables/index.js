98aa99510b2aef64690e745bc68c7a6faae50f4b
Constant Furstenberg <55072037+Congy91@users.noreply.github.com>
2023-12-28 12:12:04 +1000

    Completed Lesson 1 - Variable, Constants, Number and Arithmetic

---
+++ JavaScript/fundamentals-part-1/variables/index.js
@@ -0,0 +1,14 @@
+console.log(`${23} + ${97} = ${23 + 97}`);
+console.log(`(4 + 6 + 9) / 77 = ${(4+6+9) / 77}`);
+
+let a = 10;
+console.log(`a = ${a}`);
+console.log(`9 * a = ${9 * a}`);
+
+let b = 7 * a;
+console.log(`let b = 7 * a = ${b}`);
+
+let MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(`percentage = ${percentage}`);
